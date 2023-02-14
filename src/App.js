4717e2e4f632015e6b6f51c42e695f8ec5556030
Adriano Heissig <91757217+adrianorosset@users.noreply.github.com>
2023-02-14 08:35:40 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+import params from './params';
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o mines!</Text>
+        <Text style={styles.welcome}>
+          Tamanho da Grade: {params.getRowsAmount()} x{' '}
+          {params.getColumnsAmount()}
+        </Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+});
